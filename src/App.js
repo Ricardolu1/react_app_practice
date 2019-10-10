8902ef5c508eff3ccd80a359a570e1aebd5c79c2
Ricardo <568082704@qq.com>
2019-10-10 21:13:15 +0800

    不用react的redux的代码很乱

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React ,{Component}from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+          你点击了 <span id="value">{this.props.value}</span> 次
+        <div>
+          <button id="increment" onClick={this.add1.bind(this)}>+1</button>
+          <button id="increment2" onClick={this.add2.bind(this)}>+2</button>
+          <button id="decrement" onClick={this.minus1.bind(this)}>-1 </button>
+          <button id="incrementIfOdd" onClick={this.add3.bind(this)}>如果是单数就加一</button>
+          <button id="incrementAsync" onClick={this.add4.bind(this)}>两秒钟后加一</button>
+        </div>
+      </div>
+    )
+  }
+  add1(){
+    this.props.onadd1()
+  }
+  add2(){
+    this.props.onadd2()
+  }
+  add3(){
+    this.props.ifOdd()
+  }
+  minus1(){
+    this.props.onminus1()
+  }
+  add4(){
+    this.props.onadd4()
+  }
+
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import * as serviceWorker from "./serviceWorker"
+import {createStore} from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const statechanger = (state,action)=>{
+  if (typeof state==='undefined') {
+    return  0
+  }
+  switch (action.type) {
+    case 'add':
+      return state + action.payload
+    case 'minus':
+      return state - action.payload
+    default:
+      return state;
+  }
+}
+
+function add4() {
+  setTimeout(() => {
+    store.dispatch({type:'add',payload:1})
+  }, 2000);
+} 
+
+const store= createStore(statechanger)
+function render() {
+  ReactDOM.render(<App value={store.getState()} 
+  onadd1={()=>{store.dispatch({type:'add',payload:1})}} 
+  onadd2={()=>{store.dispatch({type:'add',payload:2})}}
+  onminus1={()=>{store.dispatch({type:'minus',payload:1})}}
+  ifOdd={()=>{
+    if (store.getState()%2===1) {
+      store.dispatch({type:'add',payload:1})
+    }
+  }}
+  onadd4={add4}
+  />,
+  
+  document.getElementById("root"))
+}
+render()
+store.subscribe(render)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
